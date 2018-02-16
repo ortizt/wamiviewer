1aa6e41f2d29f5d266749078913809a3922a9001
ortizt <ortizt@unm.edu>
2018-02-16 14:36:09 -0700

    fixed image socket sending

--- client.js
+++ client.js
@@ -1,6 +1,13 @@
 Vue.component('v-map', Vue2Leaflet.Map);
 Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
 Vue.component('v-marker', Vue2Leaflet.Marker);
+// import Dropdown from './Dropdown.vue'
+
+
+var socket = io();
+var imgChunks =[];
+
+var ctx = document.getElementById('canvas').getContext('2d');
 
 var app = new Vue({
     el: '#app',
@@ -10,7 +17,7 @@ var app = new Vue({
         center:[34, -106],
         url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        id: 'mapbox.satellite',
+        marker: L.latLng(47.218938, -1.553772),
         xcoord: null,
         ycoord: null,
         zoomnew: null
@@ -27,6 +34,20 @@ var app = new Vue({
     },
 
     created() {
+        socket.on("image", function(image, buffer) {
+            if (image) {
+              var img = new Image();
+              img.onload = function(){ // make sure img loads before drawing to canvas.
+                ctx.drawImage(img, 0, 0);
+              }
+              img.src = 'data:image/jpeg;base64,' + image.buffer;
+            }
+        });
 
+        // socket.on('img-chunk' function(chunk) {
+        //     var img = document.getElementById('img-stream2');
+        //     imgChunks.push(chunk);
+        //     img.setAttribute('src', 'data:imag/jpeg;base64,' + window.btoa(imgChunks));
+        // });
     }
 });
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
 var io = require('socket.io')(server);
+var fs = require('fs'); // required for file serving
 
 server.listen(port, function() {
     console.log('listening on *:' + port);
@@ -14,3 +15,33 @@ app.use(express.static(__dirname));
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+    console.log('A user has connected: ' +socket.id);
+    // var readStream = fs.createReadStream(path.resolve(__dirname, '/image.jpg'), {
+    //     encoding: 'binary'
+    // }), chunks = [];
+
+    // readStream.on('readable', function (){
+    //     console.log('Image loading');
+    // });
+
+    // readStream.on('data', function(chunk) {
+    //     chunks.push(chunk);
+    //     socket.emit('img-chunk', chunk);
+    // });
+
+    // readStream.on('end', function(){
+    //     console.log('Image loaded');
+    // });
+
+  });
+
+  io.on('connection', function(socket){
+    fs.readFile(__dirname + '/image.jpg', function(err, buf){
+        // it's possible to embed binary data
+        // within arbitrarily-complex objects
+        socket.emit('image', { image: true, buffer: buf.toString('base64') });
+        console.log('image file is initialized');
+    });
+  });
