5df169170623452adb7c77a82bc2f54f74d165b7
ortizt <ortizt@unm.edu>
2018-03-05 14:21:16 -0700

    cleaner img pushing

--- client.js
+++ client.js
@@ -3,20 +3,18 @@ Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
 Vue.component('v-marker', Vue2Leaflet.Marker);
 Vue.component('v-rectangle', Vue2Leaflet.Rectangle);
 Vue.component('v-image-overlay', Vue2Leaflet.ImageOverlay);
-// import Dropdown from './Dropdown.vue'
+// Vue.component('multiselect', Multiselect);
+// Vue.component('multiselect', VueMultiselect.Multiselect)
+// import Multiselect from './Dropdown.vue'
+
+
 
 
 var socket = io();
-// var imgChunks =[];
-
-var canvas = document.createElement('canvas')
-var ctx = canvas.getContext('2d');
-
-
 
 var app = new Vue({
     el: '#app',
-    
+
     data: {
         zoom:6,
         center:[34, -106],
@@ -53,67 +51,18 @@ var app = new Vue({
                 }
             }
         },
-        // dropdown() {
-        //     // console.log(this.$refs.myDropdown);
-        //     this.$refs.myDropdown.classList.toggle('show');
-        // }
     },
 
     created() {
-        // console.log(this.files)
-        // socket.on('files', function(files) {
-            // var result = Object.keys(files).map(function(key) {
-            //     // console.log(files[key]);
-            //     return files[key];
-            // });
-            // for (var i = 0; i < result[0].length; i++) {
-            //     this.files.push(result[0][i]);
-            // };
-
-        //     console.log(this.files.length)
-        //     // console.log(this.files);
-        // }.bind(this));
-        
+      
         socket.on("image", function(image, temp) {
-            // console.log(image.buffer);
-            // console.log(temp)
-            // console.log(image.buffer)
-            // var result = Object.keys(file).map(function(key) {
-            //     // console.log(files[key]);
-            //     return file[key];
-            // });
-            // for (var i = 0; i < result[0].length; i++) {
-            //     this.files.push(result[0][i]);
-            // };
-            // console.log(files)
-            // console.log(image)
             if (image) {
               var img = 'data:image/jpeg;base64,' + image.buffer;
-            //   this.imageUrl = img;
               this.imgurls.push(img);
               this.filename = temp;
-            //   console.log(this.filename)
-
               this.hash = {name: this.filename, bs64: this.imgurls};
-              console.log(this.hash);
-
-            //   console.log(this.filename)
-
-
-            //   console.log(temp)
-            //   console.log(this.imgurls)
-
-            
-            //   this.files.push(file_name);
-            //   console.log(this.imageUrl);
-            //   console.log(this.hash)
               console.log('image served');
             }
-
         }.bind(this));
-
-        //  console.log(this.files.length)
-        
-        // console.log(this.hash)
     },
 });
